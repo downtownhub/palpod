5e31c6c9a1f72bfaebbbe7f727804993101c75dc
BS <rayon@outlook.kr>
2022-02-25 19:10:52 +0900

    test'

---
+++ palpod/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from palpod.users.models import User
+from palpod.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
